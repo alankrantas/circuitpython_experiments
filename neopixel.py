c11a308ddf1450bb04267a045240b635065918ce
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2020-09-26 18:22:46 +0800

    Create neopixel.py

---
+++ neopixel.py
@@ -0,0 +1,31 @@
+# baseed on Adafruit's demo code; neopixel driver is needed
+
+import board, time, neopixel
+
+pixel_pin = board.D8
+num_pixels = 10
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+def wheel(pos):
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+while True:
+    rainbow_cycle(0)
+
